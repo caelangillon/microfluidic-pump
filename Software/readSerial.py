c7a88584be2114d21d87e6914aaed799b26603ed
caelangillon <gilloncaelan@gmail.com>
2026-02-12 06:25:41 +0000

    big update 12/02/206

--- Software/readSerial.py
+++ Software/readSerial.py
@@ -1,18 +1,39 @@
 import serial
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 from serial.tools import list_ports
 
 def read_serial():
-    port = 'COM3'
+    port = '/dev/cu.usbmodem1101'
+    data = []
     
     # Open serial connection
-    ser = serial.Serial(port, baudrate=115200, timeout=1.0)
+    ser = serial.Serial(port, baudrate=9600, timeout=1.0)
     
     try:
         while True:
             # Read and decode line
-            if line := ser.readline().decode().strip():
-                print("Received:", line)
+            if value := ser.readline().decode().strip():
+                number = float(value)
+                data.append(number)
+                print(number)
     except KeyboardInterrupt:
         ser.close()
+        return np.array(data)
 
-read_serial()
+start_time = time.time()
+data_matrix = read_serial()
+end_time = time.time()
+duration = end_time - start_time
+
+np.savetxt("frs_calib_test4.csv", data_matrix, delimiter=",")
+
+x = np.linspace(0, duration, len(data_matrix))
+print("Duration: ", duration)
+print("Length of data: ", len(data_matrix))
+plt.plot(x, data_matrix)
+plt.ylabel("Flow rate (uL/min)")
+plt.xlabel("Time (s)")
+plt.grid()
+plt.show()
---
+++ Software/read_data.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+data_matrix = np.loadtxt("frs_calib_test4.csv")
+
+# test 1: duration 1025.5775558948517 length 12983
+# test 2 (broken, do not report): duration 650.5070729255676 length 7982
+# test 3: duration 969.6334116458893 length 12267
+# test 4: duration 972.1644570827484 length 12299
+duration = 972.1644570827484
+length = 12299
+
+for i in range(len(data_matrix)):
+    if data_matrix[i] >= 200:
+        data_matrix[i] = 0
+
+xdiv = duration / 50
+x = np.linspace(0, duration, length)
+
+plt.plot(x, data_matrix)
+plt.title("Flow rate sensor calibration results")
+plt.ylabel("Flow rate (uL/min)")
+plt.xlabel("Time (s)")
+plt.ylim(0, 80)
+plt.xlim(0,duration)
+plt.grid()
+ax = plt.gca()  # get current axis
+ax.xaxis.set_major_locator(MaxNLocator(nbins=xdiv))
+ax.ticklabel_format(useOffset=False)
+ax.yaxis.set_major_locator(MaxNLocator(nbins=16))
+
+plt.show()
