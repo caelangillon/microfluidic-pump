3c0e73e825c5969eea863139c57c04571deb2c98
Yoel <yoelamos@hotmail.co.uk>
2026-03-15 12:04:06 +0000

    defined shared data for LCD code

--- Software/PDMFP/include/LCD.h
+++ Software/PDMFP/include/LCD.h
@@ -2,8 +2,9 @@
 #define LCD_H
 
 #include <Arduino.h>
+#include "sharedData.h"
 
 void setupLCD();
-void updateLCD(float pressure, float flowRate, float pressure_target, float flowRate_target, float DC1, float DC2, float time);
+void updateLCD();
 
 #endif // LCD_H
