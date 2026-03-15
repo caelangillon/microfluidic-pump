6622654fc7f40953cfa5339c00a41c3efe2ba206
Yoel <yoelamos@hotmail.co.uk>
2026-03-15 16:29:16 +0000

    tidied up

--- Software/PDMFP/src/main.cpp
+++ Software/PDMFP/src/main.cpp
@@ -7,11 +7,12 @@
 #include "controllers.h"
 #include "propValves.h"
 
-
+// Global variables for system state, controller state, PID gains and control parameters
 controlParams ctrl_params;
 pidGains pid_gains;
 systemState system_state;
 controllerState controller_state;
+
 
 void setup() {
 
@@ -29,51 +30,56 @@ void setup() {
   // setupLCD();
 }
 
+
 void loop() {
 
   // Read UI inputs
-  if (readFromUI()) {  // check if control parameters or PID gains have been changed from UI and update ctrl_params and pid_gains accordingly
+  if (readFromUI()) {                           // check if control parameters or PID gains have been changed from UI and update ctrl_params and pid_gains accordingly
     // Update control targets
     if (ctrl_params.selection == 0) {
-      // turnOffSystem(); // to be implemented: depressurizes then closes valves and stops any control actions when UI is closed
+      // turnOffSystem();                       // to be implemented: depressurizes then closes valves and stops any control actions when UI is closed
     }
     else if (ctrl_params.selection == 1) {
-      resetTargets(); // resets timer for stepped/sinusoidal control
+      system_state.Q_target = 0;                // reset flow rate target to 0 for pressure control mode
+      resetTargets();                           // resets timer for stepped/sinusoidal control
     }
     else if (ctrl_params.selection == 2) {
-      controller_state.prevFlowErr = 0; // reset PID history for flow rate controller
-      resetTargets(); // resets timer for stepped/sinusoidal control
+      controller_state.prevFlowErr = 0;         // reset PID history for flow rate controller
+      resetTargets();                           // resets timer for stepped/sinusoidal control
     }
   }
   
 
 
   // Update system states from sensors and timer
-  system_state.clock = millis() / 1000.0; // In seconds
-  updatePressure(); // reads from sensor and updates system_state.P
-  // updateFlowRate(); // reads from sensor and updates system_state.Q
+  system_state.clock = millis() / 1000.0;       // Arduino run time
+  updatePressure();                             // reads from sensor and updates system_state.P
+  // updateFlowRate();                          // reads from sensor and updates system_state.Q
 
 
 
   // Controllers
-  updateTarget();         // updates target flowrate or pressure based on control selection, type and parameters (e.g. stepped, sinusoidal, etc.)
+  updateTarget();                               // updates target flowrate or pressure based on control selection, type and parameters (e.g. stepped, sinusoidal, etc.)
   if (ctrl_params.selection == 2) {
-    controlFlowRate();      // to be implemented: calculates target pressure and updates system_state.P_target
+    controlFlowRate();                          // calculates target pressure and updates system_state.P_target if flow rate control is selected
   }
-  controlPressure();      // to be implemented: calculates valve DCs updates system_state.DC1 and system_state.DC2
+  controlPressure();                            // calculates valve DCs updates system_state.DC1 and system_state.DC2
 
 
-  // // Set valve duty cycles based on controller outputs
-  // updatePropValves(); // to be implemented: sets PWM duty cycles based on system_state.DC1 and system_state.DC2
+  // Set valve duty cycles
+  updateValvePWMs();                            // sets PWM duty cycles based on system_state.DC1 and system_state.DC2
+
+
 
   // while (!true) {
   //   resetControllers();
   // } // Optional controller reset function, could be triggered by UI?
  
 
-  // // LCD Screen
-  // updateLCD(); // updates LCD display with current system state values (pressure, flow rate, targets, valve DCs, time)
+  // LCD Screen
+  // updateLCD();                               // updates LCD display with current system state values (pressure, flow rate, targets, valve DCs, time)
 
-  // send system state back to computer for monitoring
+
+  // Send system state back to computer for monitoring
   sendToUI();
 }
