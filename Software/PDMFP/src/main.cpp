c1eef762759f6b4298aa7c805a1ecbb2a587c6d8
Yoel <yoelamos@hotmail.co.uk>
2026-03-15 11:51:55 +0000

    set up main logic

--- Software/PDMFP/src/main.cpp
+++ Software/PDMFP/src/main.cpp
@@ -29,26 +29,43 @@ void setup() {
 void loop() {
 
   // Read UI inputs
-  readFromUI(); // check if control parameters or PID gains have been changed
-
-  // System state
+  if (readFromUI()) {  // check if control parameters or PID gains have been changed
+    // Update control targets
+    if (ctrl_params.selection == 0) {
+      turnOffSystem(); // to be implemented: depressurizes then closes valves and stops any control actions when UI is closed
+    }
+    else if (ctrl_params.selection == 1) {
+      setPressureTarget(ctrl_params); // to be implemented: sets pressure target from parameters based on UI inputs
+    }
+    else if (ctrl_params.selection == 2) {
+      setFlowRateTarget(ctrl_params); // to be implemented: sets flow rate target from parameters based on UI inputs
+    }
+  }
+  
+  // Update system states from sensors and timer
   system_state.clock = millis() / 1000.0; // In seconds
-  updatePressure();
-  updateFlowRate();
+  updatePressure(); // reads from sensor and updates system_state.P
+  updateFlowRate(); // reads from sensor and updates system_state.Q
 
-  // Controller
-  system_state.DC1 = PID_V1(system_state.P_target, system_state.P);
-  system_state.DC2 = PID_V2(system_state.Q_target, system_state.Q);
+  // Controllers
+  if (ctrl_params.selection == 2) {
+      controlFlowRate(); // to be implemented: calculates target pressure and updates system_state.P_target
+      controlPressure(); // to be implemented: calculates valve DCs updates system_state.DC1 and system_state.DC2
+  }
+  else if (ctrl_params.selection == 1) {
+      controlPressure(); // to be implemented: calculates valve DCs updates system_state.DC1 and system_state.DC2
+  }
+
+  // Set valve duty cycles based on controller outputs
+  updatePropValves(); // to be implemented: sets PWM duty cycles based on system_state.DC1 and system_state.DC2
 
   while (!true) {
     resetControllers();
   } // Optional controller reset function, could be triggered by UI?
-
-  // Proportional Valves
-  updatePropValves(system_state.DC1, system_state.DC2);
+ 
 
   // LCD Screen
-  updateLCD();
+  updateLCD(); // updates LCD display with current system state values (pressure, flow rate, targets, valve DCs, time)
 
   // send system state back to computer for monitoring
   sendToUI();
