df880ce9fbc7553e35a878abe5e63dd21b7f12dd
Yoel <yoelamos@hotmail.co.uk>
2026-03-25 17:07:37 +0000

    Added ui communication capability

--- Software/PDMFP/src/UIComs.cpp
+++ Software/PDMFP/src/UIComs.cpp
@@ -1,50 +1,52 @@
 #include "UIComs.h"
 #include <Arduino.h>
 
-bool readFromUI() {
-  static uint8_t msgType = 0; // Static variable to store the message type across function calls
+uint8_t readFromUI() {
+  static uint8_t msgType = 0x00; // Static variable to store the message type across function calls
   static bool readingParams = false; // Flag to indicate if we are currently reading parameters
 
   // check for message type
   if (!readingParams) {
-    if (Serial.available() < 1) return false;
+    if (Serial.available() < 1) return 0x00; // No message received
     Serial.readBytes((char*)&msgType, 1);
     readingParams = true; 
+
+    // If message type is 0xA0, state message recived by UI, reset system_state.uiMsg to 0x00 after reading
+    if (msgType == 0xA0) {
+      readingParams = false;
+      return 0x00;
+    }
   }
 
   // if message type is 0x01, read control parameters
   if (msgType == 0x01) {
-    if (Serial.available() < (int)sizeof(ctrl_params)) return false;
+    if (Serial.available() < (int)sizeof(ctrl_params)) return 0x00; // Not enough bytes available
     Serial.readBytes((char*)&ctrl_params, sizeof(ctrl_params));
     readingParams = false;
-    return true; 
+    return msgType; 
   } 
 
   // if message type is 0x02, read PID gains
   else if (msgType == 0x02) {
     // Check if bytes are available for reading PID gains
-    if (Serial.available() < (int)sizeof(pid_gains)) return false;
+    if (Serial.available() < (int)sizeof(pid_gains)) return 0x00; // Not enough bytes available
     Serial.readBytes((char*)&pid_gains, sizeof(pid_gains));
     readingParams = false;
-    return true;
+    return msgType;
   }
 
   // if message type is  0x03, read calculated resistance
   else if (msgType == 0x03) {
     // Check if bytes are available for reading calculated resistance
-    if (Serial.available() < (int)sizeof(controller_state.R)) return false;
+    if (Serial.available() < (int)sizeof(controller_state.R)) return 0x00; // Not enough bytes available
     Serial.readBytes((char*)&controller_state.R, sizeof(controller_state.R));
     readingParams = false;
-    ctrl_params.selection = 0; // reset control selection to off
-    ctrl_params.type = 0; // reset control type to off
-    ctrl_params.param_1 = 0; // reset control parameters
-    ctrl_params.param_2 = 0;
-    ctrl_params.param_3 = 0;
-    return true;
+    return msgType;
   }
 
-  
-  return false;
+  readingParams = false;
+  msgType = 0x00;
+  return 0x00;
 }
 
 bool sendToUI() {
